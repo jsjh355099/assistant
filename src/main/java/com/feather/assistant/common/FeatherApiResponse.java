e6bf30f4e91399f368b0fceb348c050064fc7a9f
jacky_liu <jacky_liu@auer.com.tw>
2022-09-16 09:10:05 +0800

    swgger3. springfox FeatherApiResponse

---
+++ src/main/java/com/feather/assistant/common/FeatherApiResponse.java
@@ -0,0 +1,20 @@
+package com.feather.assistant.common;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponses(value = {@ApiResponse(code = 200, message = "請求成功"),
+        @ApiResponse(code = 400, message = "錯誤請求(Bad Request)"),
+        @ApiResponse(code = 401, message = "用戶停權(Unauthorized)"),
+        @ApiResponse(code = 403, message = "拒絕授權(Forbidden)"),
+        @ApiResponse(code = 404, message = "找不到路徑(Not Found)"),
+        @ApiResponse(code = 405, message = "方法不被允許(Method not allowed)"),
+        @ApiResponse(code = 500, message = "伺服器錯誤(Internal Server Error)"),
+        @ApiResponse(code = 503, message = "伺服器無法使用(Service Unavailable)")})
+public @interface FeatherApiResponse {
+
+}
--- src/main/java/com/feather/assistant/member/MemberController.java
+++ src/main/java/com/feather/assistant/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.feather.assistant.member;
 
+import com.feather.assistant.common.FeatherApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,14 +13,17 @@ public class MemberController {
     @Autowired
     MemberRepository memberRepository;
 
+    @FeatherApiResponse
     @GetMapping("/ls")
     public List<MemberEntity> getAllMember(){
         return  memberRepository.findAll();
     }
+    @FeatherApiResponse
     @GetMapping("/ls-p")
     public List<MemberEntity> getByPermission(@RequestParam(value = "permission", required = false)Integer permission){
        return  memberRepository.getByPermission(permission);
     }
+    @FeatherApiResponse
     @PostMapping("/s")
     public void setMemberRepository(@RequestBody MemberEntity memberEntity){
         if(memberRepository.existsById(memberEntity.getId_member())){
